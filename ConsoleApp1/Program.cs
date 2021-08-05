4319e5f4c489f42986d97c9c2153575c220b029c
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-05 18:44:20 +0530

    feat:QuotientandRemainder

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend : Divisor", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+
+}
+    
+

f5028724132a7fe74978f939ca0796f105c88ee7
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-05 18:56:10 +0530

    feat:even and odd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,21 +7,22 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            int number1, number2, temp;
-            Console.Write("enter the First Number : ");
-            number1 = int.Parse(Console.ReadLine());
-            Console.Write("enter the Second Number : ");
-            number2 = int.Parse(Console.ReadLine());
-            temp = number1;
-            number1 = number2;
-            number2 = temp;
-            Console.WriteLine("After Swapping : ");
-            Console.WriteLine("First Number : " + number1);
-            Console.WriteLine("Second Number : " + number2);
-            Console.ReadLine();
+            int i;
+            Console.Write("Enter a Number");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                Console.Read();
+            }
         }
-    }
 
+    }
 }
     
 

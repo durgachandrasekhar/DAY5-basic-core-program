c454500cb868938948d6500a0de69474d3e9cace
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-06 11:34:34 +0530

    feat:Leap Year or Not

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,26 +7,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            int num1, num2, num3;
-            Console.WriteLine("enter the largest number among all Three numbers");
-            Console.WriteLine("Enter the first number");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the third number");
-            num3 = Convert.ToInt32(Console.ReadLine());
-            if (num1 > num2) 
-            {
-             if (num1 > num3) 
-                Console.WriteLine("Console the number1 is greatest among all 3numbers");
-             else
-                    Console.WriteLine("Console the number3 is greater among all 3numbers");
-            }
-             else if (num2 > num3) 
-                Console.WriteLine("Console the number2 is greater among all 3numbers");
+            int year;
+            Console.WriteLine("Check weather the Year is Leap Year or Not");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 
+            Console.WriteLine("It is a LeapYear");
             else
-                    Console.WriteLine("Console the number3 is greater among all 3numbers");
-            }
+                Console.WriteLine("Not a LeapYear");
+
         }
     }
-
+}

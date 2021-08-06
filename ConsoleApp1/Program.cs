e949a7af359ecf75366d0007327b91a8cf34b5da
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-06 16:15:00 +0530

    feat:Harmonic number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,18 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            double a, b;
-            Console.WriteLine("Enter the Number : ");
-            a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Give the Exponent : ");
-            b = double.Parse(Console.ReadLine());
-            double value1 = Math.Pow(a, b);
-            Console.WriteLine("Result :" + value1);
-            Console.ReadLine();
+            int i, n;
+            double s = 0.0;
 
+            Console.WriteLine("Calculate the harmonic series and their sum: ");
+            Console.WriteLine("enter the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=n;i++)
+            {
+                Console.WriteLine("1/{0} + ",i);
+                s+=1/(float)i;
+            }
+            Console.WriteLine("sum of series upto {0} terms : {1}",n,s);
         }
     }
 }

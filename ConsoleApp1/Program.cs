7505a4e7750d734e162681cbd3dacf8a565657cf
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-06 10:13:38 +0530

    feat:check greater number among all 3 numbers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,33 +7,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            char ch;
-            Console.Write("Enter any character: ");
-            ch = Convert.ToChar(Console.ReadLine().ToLower());
-            switch (ch)
+            int num1, num2, num3;
+            Console.WriteLine("enter the largest number among all Three numbers");
+            Console.WriteLine("Enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2) 
             {
-                case 'a':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'e':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'i':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'o':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                case 'u':
-                    Console.WriteLine("The Alphabet is vowel");
-                    break;
-                default:
-                    Console.WriteLine("The Alphabet is Consonant");
-                    break;
+             if (num1 > num3) 
+                Console.WriteLine("Console the number1 is greatest among all 3numbers");
+             else
+                    Console.WriteLine("Console the number3 is greater among all 3numbers");
+            }
+             else if (num2 > num3) 
+                Console.WriteLine("Console the number2 is greater among all 3numbers");
+            else
+                    Console.WriteLine("Console the number3 is greater among all 3numbers");
             }
         }
-
     }
-}
-    
 

0ab40a39021a24862cb7849c071fb5e3f4952495
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-08 06:56:48 +0530

    feat:Flip coin with percentage

---
+++ flipcoin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace flipcoin
+{
+    class Program
+    {
+        static double FindPercentageofHeads(int numberofTimesCoinTossed)
+        {
+
+            Random rand = new Random();
+            double headsCount = 0;
+            int HEADS = 1;
+
+            for (int i = 0; i < numberofTimesCoinTossed; i++)
+            {
+                int coinTossed = rand.Next(0, 2);
+                if (coinTossed == HEADS)
+                {
+                    Console.WriteLine("Heads");
+                    headsCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+
+                }
+            }
+            return (headsCount / numberofTimesCoinTossed) * 100;
+        }
+        static double FindPercentageofTails(double headsPercentage)
+        {
+            return 100 - headsPercentage;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of times coin should be tossed: ");
+            int numberofTimesCoinTossed = int.Parse(Console.ReadLine());
+            double headsPercentage = FindPercentageofHeads(numberofTimesCoinTossed);
+            double tailsPercentage = FindPercentageofTails(headsPercentage);
+            Console.WriteLine("Heads Percentage :" + headsPercentage);
+            Console.WriteLine("Tails Percentage :" + tailsPercentage);
+        }
+    }
+}
